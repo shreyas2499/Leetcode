36ad743eb20f8938ca3ced9bda64c8cf8e9ced83
shreyas2499 <59840906+shreyas2499@users.noreply.github.com>
2022-10-11 14:55:24 +0530

    Time: 66 ms (33.00%), Space: 13.9 MB (62.83%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,21 +1,6 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        # for i in range(len(nums)):
-        #     if(nums[i] == val):
-        #         nums.replace(nums[i],"_")
-        test = []
-        
         for i in range(len(nums)):
             if(val in nums):
                 nums.remove(val)
-        
-        # for i in nums:
-        #     if(i==val):
-        #         # nums[index(i)]="_"
-        #         nums.remove(i)
-        #         # if(i<len(nums)-1):
-        #         #     nums[i] = nums[i+1]
-        #         # nums.pop(index(nums[i]))
-        # nums.remove(val)
-        
         return len(nums)
