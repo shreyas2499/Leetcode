34ba90c9bc2aeabc6412f25e2367df6c895d27d2
shreyas2499 <59840906+shreyas2499@users.noreply.github.com>
2022-10-22 19:43:36 +0530

    Time: 6381 ms (11.40%), Space: 14.9 MB (78.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,24 +5,11 @@ class Solution:
         for i in nums:
             ad.append(nums.index(i))
             dummy = dummy - i
-            # print(dummy)
             nums[nums.index(i)] = ''
-            # print(nums)
             if(dummy in nums):
                 ad.append(nums.index(dummy))
-                # print(ad)
                 return ad
             else:
                 ad = []
                 dummy = target
         return ad
-            
-#         sum = 0
-#         ad = []
-#         for i in range(len(nums)):
-#             for j in range(i+1,len(nums)):
-#                 sum = nums[i]+nums[j]
-#                 if(sum == target):
-#                     ad.append(nums.index(nums[i]))
-#                     ad.append(nums.index(nums[j]))
-#         return ad
