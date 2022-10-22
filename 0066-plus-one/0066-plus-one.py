bba76e2670a7de495e854d61552ecea20481a8c7
shreyas2499 <59840906+shreyas2499@users.noreply.github.com>
2022-10-22 22:32:57 +0530

    Time: 61 ms (46.46%), Space: 13.9 MB (59.02%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,10 +1,11 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        s = ''
+        # s = ''
+        n = 0
         for i in digits:
-            s = s + str(i)
+            n = n*10 + i
         
-        n = int(s)
+        # n = int(s)
         n = n + 1
         b = str(n)
         return list(b)
