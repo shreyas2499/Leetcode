f2e17e0471ee2a35c669aadd15a1bafa3d570023
shreyas2499 <59840906+shreyas2499@users.noreply.github.com>
2023-06-18 23:30:03 +0530

    Time: 147 ms (27.08%), Space: 17.2 MB (85.55%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,26 +1,33 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        dummy = target
-        nums = numbers
-        ad = []
-        # nums = []
-        # for i in numbers:
-            
-                # nums.append(i)
+#         Pointer approach
+        l, r = 0, len(numbers)-1
+    
+        while(l<=r):
+            if(numbers[l]+numbers[r] > target):
+                r = r - 1
+            if(numbers[l]+numbers[r] < target):
+                l = l + 1
+            if(numbers[l]+numbers[r] == target):
+                return [l+1,r+1]
+        return []
         
-        # print(set(nums))
-        abcd = nums
-        for i in list(set(nums)):
-            # if(i<=target):
-            print(i, nums.index(i))
-            ad.append(nums.index(i)+1)
-            dummy = dummy - i
+        
+    
+#         Brute force approach
+#         dummy = target
+#         nums = numbers
+#         ad = []
+#         for i in list(set(nums)):
+#             print(i, nums.index(i))
+#             ad.append(nums.index(i)+1)
+#             dummy = dummy - i
 
-            nums[nums.index(i)] = ''
-            if(dummy in list(set(nums))):
-                ad.append(nums.index(dummy)+1)
-                return sorted(ad)
-            else:
-                ad = []
-                dummy = target
-        return sorted(ad)
+#             nums[nums.index(i)] = ''
+#             if(dummy in list(set(nums))):
+#                 ad.append(nums.index(dummy)+1)
+#                 return sorted(ad)
+#             else:
+#                 ad = []
+#                 dummy = target
+#         return sorted(ad)
