4ec8e65e74f4ec4baa23c5ccf3972d39c778c18b
shreyas2499 <59840906+shreyas2499@users.noreply.github.com>
2023-06-22 22:11:27 +0530

    Time: 734 ms (57.19%), Space: 29.4 MB (36.76%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,19 +1,14 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-#         I need second heightest index - heightest index for width
-#         I need second heightest index
-        
         l = 0
         r = len(height) - 1
-        area = []
+        area = (r - l) * min(height[l], height[r])
         while l<r:
-            # print(height[l], height[r])
-            area.append((r - l) * min(height[l], height[r]))
+            area = max(area, (r - l) * min(height[l], height[r]))
             if(height[l]<height[r]):
                 l = l + 1
             elif(height[l]>height[r]):
                 r = r - 1
             elif(height[l]==height[r]):
                 l = l + 1
-        print(area)
-        return max(area)
+        return area
