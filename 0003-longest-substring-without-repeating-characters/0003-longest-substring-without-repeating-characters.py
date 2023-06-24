45e6ae6f9529c8a99091d96914f1194dc9549e92
shreyas2499 <59840906+shreyas2499@users.noreply.github.com>
2023-06-25 00:42:18 +0530

    Time: 74 ms (71.51%), Space: 16.2 MB (96.00%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,18 +1,36 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        l, r = 0, 0
-        arr = ""
-        maxS = 0
-        while r<len(s):
-            if(s[r] not in arr):
-                arr = arr + s[r]
-                maxS = max(maxS, len(arr))
-                r = r + 1
-            else:
-                arr = ""
+        # Optimised solution
+        charSet = set()
+        l = 0
+        res = 0
+        
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
                 l = l + 1
-                r = l
-        print(arr)
-        print(maxS)
-        return maxS
+            charSet.add(s[r])
+            res = max(res, r-l+1)
+                    
+        return res
+        
+
+        
+        
+        #         First Solution
+        # l, r = 0, 0
+        # arr = ""
+        # maxS = 0
+        # while r<len(s):
+        #     if(s[r] not in arr):
+        #         arr = arr + s[r]
+        #         maxS = max(maxS, len(arr))
+        #         r = r + 1
+        #     else:
+        #         arr = ""
+        #         l = l + 1
+        #         r = l
+        # # print(arr)
+        # # print(maxS)
+        # return maxS
         
