92434009ea655e236c3e2b20281a94d5c89bfe1e
shreyas2499 <59840906+shreyas2499@users.noreply.github.com>
2023-06-25 14:46:13 +0530

    Time: 689 ms (8.97%), Space: 17.1 MB (83.38%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -8,15 +8,9 @@ class Solution:
         
         for r in range(len(nums)):
             count[nums[r]] = count.get(nums[r], 0) + 1
-            print(count)
-            print(r-l+1)
             if(((r-l+1) - count.get(1, 0) > k)):
-                print(r-l+1)
-                print(count)
                 count[nums[l]] -= 1
-                # print(count, "reduced\n")
                 l = l + 1
-            # elif(nums[r] == 0):
             
             res = max(res, r-l+1)
         return res
