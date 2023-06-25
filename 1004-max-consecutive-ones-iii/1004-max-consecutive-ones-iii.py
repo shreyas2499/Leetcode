7a89534772d7e4bcbe1940508dab4a1e05a39084
shreyas2499 <59840906+shreyas2499@users.noreply.github.com>
2023-06-25 14:47:50 +0530

    Time: 675 ms (10.90%), Space: 17.2 MB (41.84%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -9,7 +9,7 @@ class Solution:
         for r in range(len(nums)):
             count[nums[r]] = count.get(nums[r], 0) + 1
             # if(((r-l+1) - count.get(1, 0) > k)):
-            if(count.get(0, 0) > k):
+            while(count.get(0, 0) > k):
                 count[nums[l]] -= 1
                 l = l + 1
             
