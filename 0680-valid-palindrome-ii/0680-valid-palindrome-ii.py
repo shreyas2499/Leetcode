55ce89c69cda02ae78cf804d86b125e7a833818e
shreyas2499 <59840906+shreyas2499@users.noreply.github.com>
2024-03-16 14:27:58 -0400

    Time: 108 ms (42.38%), Space: 17.1 MB (36.06%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -10,12 +10,6 @@ class Solution:
                 r-=1
             else:
                 if(skip == 1):
-                    # print(s[l+1: r])
-                    # print(s[l+1: r][::-1])
-                    # print(s[l])
-                    # print(s[r])
-                    # print(s[l:r-1])
-                    # print(s[l:r-1][::-1])
                     if(s[l+1: r+1] == s[l+1: r+1][::-1]):
                         l+=1
                         skip = 0
