0c996c6101d2f8ef4f7d2cf7cde1b739cba96eac
shreyas2499 <59840906+shreyas2499@users.noreply.github.com>
2024-03-19 14:14:31 -0400

    Time: 58 ms (41.09%), Space: 20.4 MB (56.41%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.py
@@ -0,0 +1,39 @@
+class MinStack:
+
+    """
+        Use two stacks. One for the actual values and one to store the min for each index
+        getMin returns the top of the minStack array
+        getTop returns the top of the value array
+    """
+    
+    
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+        
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
+        
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+        # self.minStack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
